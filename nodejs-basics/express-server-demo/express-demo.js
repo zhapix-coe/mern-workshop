715d1a977f38ce6859285d275d689485b0ab29d6
Vijayan T <69061573+vijayancpt@users.noreply.github.com>
2025-07-24 21:22:26 +0530

    updated express routing

--- nodejs-basics/express-server-demo/express-demo.js
+++ nodejs-basics/express-server-demo/express-demo.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const port  = 5252;
+const port = 5252;
 
-
+// Initialise userList to manage users
 const userList = [];
 
 // To add user data to the User List
@@ -12,8 +12,9 @@ const addUser = (userData) => {
 
 // Sample user
 const newUser = {
-  name: "John",
-  age: 20,
+  userId: 1,
+  userName: "John",
+  userAge: 20,
 };
 
 addUser(newUser);
@@ -22,43 +23,68 @@ addUser(newUser);
 app.use(express.static("public"));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Welcome to IRP program - ExpressJS!')
-})
+// Routing for the Endpoints
+app.get("/", (req, res) => {
+  res.send("Welcome to IRP program - ExpressJS Demo!");
+});
+// Read all users
+app.get("/users", (req, res) => {
+  res.json(userList);
+});
+// Create an user
+app.post("/users", (req, res) => {
+  const { userName, userAge } = req.body;
+  const newUserId = userList.length + 1;
+  addUser({ userId: newUserId, userName, userAge });
+  res.status(200).send({ message: `Name:${userName} is added Succcessfully!` });
+});
 
+// To Update a User data. 
+// http://zhapix.com/users/23.    ----> PUT
+app.put("/users/:id", (req, res) => {
+  console.log("Entering into Update User");
+  
+  const userId = parseInt(req.params.id);
+  const { userName, userAge } = req.body;
+  const userData = userList.find((curUser) => curUser.userId == userId);
+  userData.userName = userName;
+  userData.userAge = userAge;
+  res.json({ message: `Name:${userName} is updated Succcessfully!` });
+});
 
-app.get('/user-list', (req, res) => {  
-  res.json(userList)
-})
+// To delete an user
+app.delete("/users/:id", (req, res) => {
+  const userId = req.params.id;
+  const userIndex = userList.findIndex((curUser) => curUser.userId == userId);
 
+  const deletedUser = userList.splice(userIndex, 1)[0];
+  console.log("deletedUser:",deletedUser);
+  
+  res.json({
+      message: `Name:${deletedUser.userName} is deleted Succcessfully!`,
+    });
+  
+});
 
 // To Simulate an error route
-app.get('/error', (req, res, next) => {
-  const err = new Error('Something went wrong!');
+app.get("/error", (req, res, next) => {
+  const err = new Error("Something went wrong!");
   err.status = 500;
   next(err); // Pass error to the error handler
 });
 
-
-
 // Custom error handler middleware (must have 4 args)
 
-const handleError =(err, req, res, next) => {
-  console.error('Error:', err.message);
+const handleError = (err, req, res, next) => {
+  console.error("Error:", err.message);
   res.status(err.status || 500).json({
     success: false,
-    message: err.message || 'Internal Server Error',
+    message: err.message || "Internal Server Error",
   });
-}
+};
 
 app.use(handleError);
 
-
-
-
-app.listen(port,()=>{
-    console.log("Express Demo Server Started:::5252");    
-})
-
-
-// npm install -g nodemon
+app.listen(port, () => {
+  console.log("Express Demo Server Started:::5252");
+});
